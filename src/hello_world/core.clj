f04355f8679fe6395aa0ae0bd2e6fed6b3dac56c
Juraj Martinka <jumarko@gmail.com>
2017-05-31 15:43:56 +0200

    Modify hello-world namespace to have something in history.

--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -1,7 +1,13 @@
 (ns hello-world.core
   (:gen-class))
 
+(defn fun [a b]
+  (println "I'm feeling good today")
+  (+ a b))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (fun [10 20]))
+
